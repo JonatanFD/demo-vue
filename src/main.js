0153783bd335cdbc36bd66cdc2e27c5de1f1db4e
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-05-19 15:09:36 -0500

    add componentns

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { Button, Card, ConfirmationService, DialogService, SelectButton, ToastService } from "primevue";
+import { Button, Card, Checkbox, Column, ConfirmationService, ConfirmDialog, DataTable, Dialog, DialogService, Drawer, FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu, Rating, Row, Select, SelectButton, Tag, Textarea, Toast, ToastService, Toolbar } from "primevue";
 import I18n from "./i18n";
 import { PrimeVue } from "@primevue/core";
 import Aura from "@primeuix/themes/aura";
@@ -19,4 +19,24 @@ createApp(App)
     .component("pv-card", Card)
     .component("pv-select-button", SelectButton)
     .component("pv-button", Button)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select', Select)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-text', InputText)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
     .mount("#app");
